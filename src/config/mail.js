6b3fcd48c05f052b064d7b3c94fff22e7aaf635d
bruss86 <leandroger@gmail.com>
2026-07-11 19:54:48 -0300

    enviar tareas 8

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,13 +1,17 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  host: "smtp.gmail.com",
+  port: 587,
   secure: false,
+
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+
+  family: 4, // Forzar IPv4
 });
 
 module.exports = transporter;
